9ef24e67e0dc5ebff4f2917ac5db8e3bfdb4feca
Takhir <guvalakat1603@gmail.com>
2025-08-27 18:10:23 +0500

    Docker demo

---
+++ src/main/java/uz/pdp/dockerdemo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package uz.pdp.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/home")
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Springboot with Docker Application!";
+    }
+}
