3b7e3198f28ee93d41d0500d24bbabd03dfc1ba3
Takhir <guvalakat1603@gmail.com>
2025-09-03 16:49:52 +0500

    postgres

---
+++ src/main/java/uz/pdp/dockerdemo/Student.java
@@ -0,0 +1,21 @@
+package uz.pdp.dockerdemo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private Integer age;
+}
